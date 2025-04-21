aabc02ea0db613cde88e0d4a7db1b6244c91501a
Kevin420-KS <kevinsebastian42069@gmail.com>
2025-04-21 21:17:36 +0700

    Teori & Praktikum

--- pert3/src/app/Livewire/ShowHomePage.php
+++ pert3/src/app/Livewire/ShowHomePage.php
@@ -3,9 +3,17 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Footer;
 
 class ShowHomePage extends Component {
+    
+    public $foot;
+
+    public function mount (){
+        $this->foot = Footer::first();
+    }
+
     public function render(){
-        return view('livewire.show-home-page');
+        return view('livewire.show-home-page', ['footer' => $this->foot]);
     }
 }
